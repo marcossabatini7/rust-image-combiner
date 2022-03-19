b380a5bf83c78a3cf2c5f6f95f67670c3c7bd927
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 18:12:39 -0300

    def fn find image from path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod args;
+use std::{fs::File, io::BufReader};
+
 use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat};
 
 fn main() {
     let args = Args::new();
+    let (image1, image_format1) = find_image_from_path(args.image1);
+    let (image2, image_format2) = find_image_from_path(args.image2);
+}
 
-    println!("{:?}", args);
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
 }
