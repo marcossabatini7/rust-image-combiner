38d255213619ac1b891b10374978bba70a1504ab
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-19 18:17:21 -0300

    impl ImageDataErrors enum for return

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,20 @@ use std::{fs::File, io::BufReader};
 use args::Args;
 use image::{io::Reader, DynamicImage, ImageFormat};
 
-fn main() {
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
     let (image1, image_format1) = find_image_from_path(args.image1);
     let (image2, image_format2) = find_image_from_path(args.image2);
+
+    if image_format1 != image_format2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+    Ok(())
 }
 
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
